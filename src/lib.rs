a434d0db588444f919e93115ed65bd75407fc552
Austin Schey <aschey13@gmail.com>
2024-06-04 17:48:18 -0500

    chore: internal refactors (#3)

--- src/lib.rs
+++ src/lib.rs
@@ -18,40 +18,4 @@ mod parse;
 mod reader;
 
 pub use headers::*;
-use http::HeaderMap;
 pub use reader::*;
-
-/// Header name to request icy metadata.
-pub const ICY_METADATA_HEADER: &str = "Icy-MetaData";
-
-/// Appends the `Icy-MetaData` header to the `header_map`.
-pub fn add_icy_metadata_header(header_map: &mut HeaderMap) {
-    header_map.append(
-        ICY_METADATA_HEADER,
-        "1".parse().expect("valid header value"),
-    );
-}
-
-/// Trait for requesting icy metadata from an HTTP request builder
-pub trait RequestIcyMetadata {
-    /// Appends the `Icy-MetaData` header to the request's header map
-    fn request_icy_metadata(self) -> Self;
-}
-
-#[cfg(feature = "reqwest")]
-impl RequestIcyMetadata for reqwest::ClientBuilder {
-    fn request_icy_metadata(self) -> Self {
-        let mut header_map = HeaderMap::new();
-        add_icy_metadata_header(&mut header_map);
-        self.default_headers(header_map)
-    }
-}
-
-#[cfg(feature = "reqwest")]
-impl RequestIcyMetadata for reqwest::RequestBuilder {
-    fn request_icy_metadata(self) -> Self {
-        let mut header_map = HeaderMap::new();
-        add_icy_metadata_header(&mut header_map);
-        self.headers(header_map)
-    }
-}
