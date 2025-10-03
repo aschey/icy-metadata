a3ccd6f8a07edb5c78d41fd4bbf791a7d6c80cd0
Austin Schey <aschey13@gmail.com>
2025-10-02 22:36:51 -0700

    chore: remove doc_auto_cfg (#71)

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
 #![forbid(clippy::unwrap_used)]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![doc = include_str!("../README.md")]
 
 pub mod error;
